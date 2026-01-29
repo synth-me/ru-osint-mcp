7404bf736fddfb01adb19d75a74f91395c71d1c6
Muriel Panegassi <murielpanegassi1@gmail.com>
2026-01-29 14:14:57 +0900

    Debugging for Horizon

--- ru-osint-mcp/modules/Authentication.py
+++ ru-osint-mcp/modules/Authentication.py
@@ -12,6 +12,9 @@ DB_NAME = "../authentication-server/tokens.sqlite"
 
 class SQLiteAuthMiddleware(Middleware):
     def verify_token(self, token: str) -> bool:
+
+        print(f"Token is: {token}")
+        
         if not token:
             return False
         
@@ -28,11 +31,12 @@ class SQLiteAuthMiddleware(Middleware):
 
     async def on_call_tool(self, context: MiddlewareContext, call_next):
         headers = get_http_headers()
-        
+        print(headers)
         token = headers.get("Authorization") if headers else os.getenv("TOKEN")
-
+        print(f"Token is {token}")
         if not self.verify_token(token):
             raise ToolError("Unauthorized: Invalid or missing secure token.")
 
 
         return await call_next(context)
+
